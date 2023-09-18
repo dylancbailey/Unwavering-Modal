8fee8baa192aca63918de587b39880b5c829f380
Dylan Bailey <wearesleepless@gmail.com>
2023-09-18 17:01:14 -0400

    preventDefault

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const modalEl = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
+const formEl = document.getElementById("consent-form")
 
 modalCloseBtn.addEventListener("click", function() {
     modalEl.style.display = "none"
+})
+
+formEl.addEventListener("submit", function(e) {
+    e.preventDefault()
 })
 
 setTimeout(function() {
