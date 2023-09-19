a281d8ff4cb268faa8eff4e94a08c8e63428448d
Dylan Bailey <wearesleepless@gmail.com>
2023-09-19 09:09:10 -0400

    Form data

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const modalEl = document.getElementById("modal")
+const modalEl       = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
-const modalText = document.getElementById("modal-text")
-const formEl = document.getElementById("consent-form")
+const modalText     = document.getElementById("modal-text")
+const formEl        = document.getElementById("consent-form")
 
 modalCloseBtn.addEventListener("click", function() {
     modalEl.style.display = "none"
@@ -9,14 +9,34 @@ modalCloseBtn.addEventListener("click", function() {
 
 formEl.addEventListener("submit", function(e) {
     e.preventDefault()
-    modalText.innerHTML = `
-    <div class="modal-inner-loading">
-        <img src="images/loading.svg" class="loading">
-        <p id="uploadText">
-            Uploading your data to the dark web...
-        </p>
-    </div>
-` 
+
+    const formData = new FormData(formEl)
+    const formName = formData.get("formName")
+
+    modalText.innerHTML = 
+    `
+        <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="uploadText">
+                Uploading your data to the dark web...
+            </p>
+        </div>
+    ` 
+    setTimeout(() => {
+        document.getElementById("uploadText").innerText = "Making the sale..."
+    }, 1500);
+
+    setTimeout(() => {
+        document.getElementById("modal-inner").innerHTML = 
+        `
+        <h2>Thanks <span class="modal-display-name">${formName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+        `
+    }, 3000);
+
 })
 
 setTimeout(function() {
